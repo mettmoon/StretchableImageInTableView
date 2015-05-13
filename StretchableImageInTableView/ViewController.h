8a4e905415c3948add2bfac25e2c806b8e8f932e
mettmoon <ys_qwerty700@naver.com>
2015-05-13 10:55:38 +0900

    Initial Commit

---
+++ StretchableImageInTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StretchableImageInTableView
+//
+//  Created by Hayden on 2015. 5. 13..
+//  Copyright (c) 2015년 Hayden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StretchableImageInTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StretchableImageInTableView
+//
+//  Created by Hayden on 2015. 5. 13..
+//  Copyright (c) 2015년 Hayden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
